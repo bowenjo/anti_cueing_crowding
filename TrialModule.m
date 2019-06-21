5689f16b2cd88b9c0307688546bdced06f660988
Joel Bowen <joel_bowen@berkeley.edu>
2019-06-20 18:39:23 -0700

    adds the base class for running trial modules

---
+++ TrialModule.m
@@ -0,0 +1,39 @@
+classdef TrialModule
+    %MODULE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        window  
+        xCenter
+        yCenter
+        expDesign
+    end
+    
+    methods
+        function self = TrialModule(window, windowRect)
+            %MODULE Construct an instance of this class
+            %   Detailed explanation goes here
+            self.window = window;
+            [self.xCenter, self.yCenter] = RectCenter(windowRect);
+            self.expDesign = containers.Map();
+        end
+        
+        function [] = draw_fixation(self)
+            Screen('DrawDots', self.window, [self.xCenter; self.yCenter],...
+                10, 0, [], 2);
+        end
+        
+        function [] = run(self, nTrials)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            % set the experimental design
+            self.set_exp_design(nTrials);
+            % run the design for each trial
+            vbl = Screen('Flip', self.window);
+            for i = 1:nTrials
+                self.forward(i, vbl)
+            end
+        end
+    end
+end
+
