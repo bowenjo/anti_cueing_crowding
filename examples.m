3e2d1dcd806afd62e957ac42eef439637290189b
Joel Bowen <joel_bowen@berkeley.edu>
2019-07-08 17:35:21 -0700

    updates the examples script for the eyelink additions

--- examples.m
+++ examples.m
@@ -18,36 +18,18 @@ utilsPath = [pwd '/utils'];
 addpath(utilsPath)
 
 %% Fixed Params
-cuedGrey = black;
-nonCuedGrey = white;
-
-% buld rectangles
-baseRect = [0 0 400 200]; % base rectangle size in pixels
-
 % timing info
 isiTime = 1; % pre-cue time
 cueTime = 200/1000; % cue presentation time
 stimTime = 133/1000; % stimulus presentation time
-
-% rectangle cueing information
-postCuedRect = [2 1]; % post cue rectangle indices to present stimulus when valid
 cuedRectProb = [.5 .5]; % probability of cue location
-cueLineSize = 6;
-lineSize = 2;
 
 % orientation information
 orientProb = [.5 .5];
 orientCh = [45 135];
 
-% grating info
-spatialFrequency = .03;
-diameter = 80;
-contrast = 1;
-
-
 %% Example Experiment 1: Informative/ long SOA
-% Independent variablesSCA
-
+% Independent variables
 soaTime = 600/1000; %600 ms
 cueValidProb = .8; % probability the cue is valid
 spaceCh = [100, 80]; % define your spacing choices in pixels
@@ -55,18 +37,13 @@ spaceProb = [1, 0]; % decide the proportion of each choice
 
 % Make the widow and get the rectangle postions
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, backgroundGrey);
-[screenXpixels, screenYpixels] = Screen('WindowSize', window);
-squareXpos = [.25 .75] .* screenXpixels;
-squareYpos = [.5 .5] .* screenYpixels;
 
 % initialize the block of trials module with the parameters
-Block1 = CueRects(window,windowRect, baseRect, squareXpos, squareYpos, cuedGrey,...
-    nonCuedGrey, cueLineSize, lineSize, postCuedRect, cuedRectProb, cueValidProb, ...
+Block1 = CueRects(window, windowRect, cuedRectProb, cueValidProb, ...
     spaceProb, orientProb, spaceCh, orientCh, ...
-    spatialFrequency, diameter, contrast, ...
     isiTime, cueTime, soaTime, stimTime);
 
-% Block1.run(10) % run 5 example trials from the block
+% Block1.run(10, 'block_1') % run 5 example trials from the block
 sca;
 % Block1.results
 % Block1.expDesign('T_orient')
@@ -82,10 +59,8 @@ spaceProb = [.5, .5]; % decide the proportion of each choice (just doing one spa
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, backgroundGrey);
 
 % initialize the block of trials module
-Block2 = CueRects(window, windowRect, baseRect, squareXpos, squareYpos, cuedGrey,...
-    nonCuedGrey, cueLineSize, lineSize, postCuedRect, cuedRectProb, cueValidProb, ...
+Block2 = CueRects(window,windowRect, cuedRectProb, cueValidProb, ...
     spaceProb, orientProb, spaceCh, orientCh, ...
-    spatialFrequency, diameter, contrast, ...
     isiTime, cueTime, soaTime, stimTime);
 
 % Block2.run(5) % run 5 example trials from the block
@@ -100,23 +75,20 @@ sca;
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, backgroundGrey);
 Exp = Experiment();
 
-% Build a waitscreen
-continueMessage = 'Press the Space Bar to continue';
-Wait1 = WaitScreen(window, continueMessage, 70);
-% append blocks and corresponding number of trials
-Exp.append_block('1', Block1, 10) % Block 1: Informative/Long SOA for 10 trials
-Exp.append_block('2', Wait1, 0)
-Exp.append_block('3', Block2, 10)  % Block 2: Non-informative/ Short SOA for 10 trials
-Exp.run() % run all blocks
+% Build a waitscreen 
+iMessage = 'Informative  \n press space bar to start';
+nMessage = 'Non-Informative \n press space bar to start'; 
+iWait = WaitScreen(window, iMessage, 70); 
+nWait = WaitScreen(window, nMessage, 70);   
+% append blocks and corresponding number of tri a ls 
+Exp.append_block('1', iWait, 0) 
+Exp.append_block('2', Block1, 2) % Block 1: Informative/Long SOA for 10 trials
+Exp.append_block('3', nWait, 0) 
+Exp.append_block('4', Block2, 2 )  % Block 2: Non-informative/ Short SOA  for 10 trials
+Exp.run() % run all blocks   
+sca; 
+  
+% TODOs: 
+% need to decide when to check fixations DURING a trial
+% Choose the fixed params correctly and change them in CuedRectParams
 
-sca;
-
-% NOTES:
-% The sizes are not the ones we'll use in the experiments. I just chose
-% these ones because I do not know what they should be yet cause I haven't
-% looked at the monitor in the eye-tracking room.
-
-% TODOs:
-% 1) Still need to add code to obtain and measure subject responses.
-% 2) Need to add in instruction/break screens between blocks
-% 3) Need to add in eye-tracking stuff 
