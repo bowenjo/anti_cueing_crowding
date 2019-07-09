eb422bd6d4a05d1900c6d5606eb58633e5b0ba40
Joel Bowen <joel_bowen@berkeley.edu>
2019-07-09 16:25:50 -0700

    updates examples so lines aren't commented out

--- examples.m
+++ examples.m
@@ -11,7 +11,7 @@ PsychDefaultSetup(2);
 screens = Screen('Screens');
 screenNumber = max(screens);
 white = WhiteIndex(screenNumber);
-black = BlackIndex(screenNumber);
+black = BlackIndex(screenNumber);  
 backgroundGrey = white / 2;
 
 utilsPath = [pwd '/utils'];
@@ -25,14 +25,15 @@ stimTime = 133/1000; % stimulus presentation time
 cuedRectProb = [.5 .5]; % probability of cue location
 
 % orientation information
-orientProb = [.5 .5];
 orientCh = [45 135];
+orientProb = [.5 .5];
+
 
 %% Example Experiment 1: Informative/ long SOA
 % Independent variables
 soaTime = 600/1000; %600 ms
 cueValidProb = .8; % probability the cue is valid
-spaceCh = [100, 80]; % define your spacing choices in pixels
+spaceCh = [0, 100]; % define your spacing choices in pixels
 spaceProb = [1, 0]; % decide the proportion of each choice  
 
 % Make the widow and get the rectangle postions
@@ -43,17 +44,18 @@ Block1 = CueRects(window, windowRect, cuedRectProb, cueValidProb, ...
     spaceProb, orientProb, spaceCh, orientCh, ...
     isiTime, cueTime, soaTime, stimTime);
 
-% Block1.run(10, 'block_1') % run 5 example trials from the block
+Block1.run(5, 'block_1') % run 5 example trials from the block
 sca;
-% Block1.results
-% Block1.expDesign('T_orient')
+
+% Block1.results % result matrix (responseKeys, responseTimes, fixations)
+% Block1.expDesign('T_orient') % ground truth target orientation
 
 %% Example Experiment 2: Non-Informative/ short SOA
 % Independent variables
 soaTime = 40/1000; % 40ms
 cueValidProb = .5; % probability the cue is valid
-spaceCh = [100, 200]; % define your spacing choices in pixels
-spaceProb = [.5, .5]; % decide the proportion of each choice (just doing one spacing for example purposes)
+spaceCh = [0, 100]; % define your spacing choices in pixels
+spaceProb = [.5, .5]; % decide the proportion of each choice 
 
 % Make the widow 
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, backgroundGrey);
@@ -63,7 +65,7 @@ Block2 = CueRects(window,windowRect, cuedRectProb, cueValidProb, ...
     spaceProb, orientProb, spaceCh, orientCh, ...
     isiTime, cueTime, soaTime, stimTime);
 
-% Block2.run(5) % run 5 example trials from the block
+Block2.run(5, 'block_2') % run 5 example trials from the block
 sca;
 
 %% Build full experiments
@@ -75,20 +77,20 @@ sca;
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, backgroundGrey);
 Exp = Experiment();
 
-% Build a waitscreen 
-iMessage = 'Informative  \n press space bar to start';
-nMessage = 'Non-Informative \n press space bar to start'; 
+% Build a waitscreen  
+iMessage = 'Informative Block  \n \n press space bar to start';
+nMessage = 'Non-Informative Blcok \n \n press space bar to start'; 
 iWait = WaitScreen(window, iMessage, 70); 
 nWait = WaitScreen(window, nMessage, 70);   
-% append blocks and corresponding number of tri a ls 
-Exp.append_block('1', iWait, 0) 
-Exp.append_block('2', Block1, 10) % Block 1: Informative/Long SOA for 10 trials
-Exp.append_block('3', nWait, 0) 
-Exp.append_block('4', Block2, 10)  % Block 2: Non-informative/ Short SOA for 10 trials
+% append blocks and corresponding number of trials 
+Exp.append_block('1_wait', iWait, 0) 
+Exp.append_block('2_block', Block1, 2) % Block 1: Informative/Long SOA for 10 trials
+Exp.append_block('3_wait', nWait, 0) 
+Exp.append_block('4_block', Block2, 2)  % Block 2: Non-informative/ Short SOA for 10 trials
 Exp.run() % run all blocks   
 sca; 
-  
+ 
 % TODOs: 
-% need to decide when to check fixations DURING a trial
 % Choose the fixed params correctly and change them in CuedRectParams
+% Choose a good way to save all of the results matrices for each block
 
