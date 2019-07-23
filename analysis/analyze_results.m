72efb7b95cb5beb719b0a28e1384244a8842de83
Joel Bowen <joel_bowen@berkeley.edu>
2019-07-23 14:17:44 -0700

    renames the test analysis function

--- analysis/analyze_results.m
+++ analysis/analyze_results.m
@@ -1,6 +1,6 @@
-function outputTable = testAnalysis(resultsTable)
+function outputTable = analyze_results(resultsTable, filter_idx, filter_value)
     % load in the results table and calculate accuracy and reaction time
-    % Table row indices:
+    % resultsTable row keys:
     %   1. resp key
     %   2. RT
     %   3. fix_check
@@ -19,11 +19,19 @@ function outputTable = testAnalysis(resultsTable)
 
     col = 1;
     for sp = spacing
+        % indices for given spacing
         spIdx = resultsMat(5,:) == sp;
-        spAccuracy = resultsMat(9, spIdx);
-        spRT = resultsMat(2, logical(spIdx .* resultsMat(9,:)));
-
-        accuracy(col) = mean(spAccuracy);
+        % get accuracy for given spacing
+        spCorrect = resultsMat(9, spIdx); 
+        % only get RT for correct trials
+        spRT = resultsMat(2, filter_by_index(9, 1, spIdx, resultsMat));
+%         
+%         if filter_idx ~= 0
+%             
+%         end
+%         
+        % collect and compute accuracy
+        accuracy(col) = mean(spCorrect);
         reaction_time(col) = mean(spRT);
         col = col+1;
     end
@@ -31,4 +39,9 @@ function outputTable = testAnalysis(resultsTable)
     outputTable = table(spacing', accuracy', reaction_time', ...
         'VariableNames', {'Spacing', 'Accuracy', 'Reaction_Time'});
 %     save('results/work_spacing_2flank_tang', 'outputTable')
+end
+
+function filter_indices = filter_by_index(idx, value, spIdx, resultsMat)
+    value_filter_indices = resultsMat(idx,:) == value;
+    filter_indices = logical(spIdx .* value_filter_indices);
 end
