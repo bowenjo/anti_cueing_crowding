8017f39ef9ce4275fb0503d7a83380bf600aa930
Joel Bowen <joel_bowen@berkeley.edu>
2019-09-17 16:20:06 -0700

    adds a simple script for plotting results

---
+++ display_notebook.m
@@ -0,0 +1,31 @@
+%% load in data
+utilsPath = [pwd '/utils'];
+analysisPath = [pwd '/analysis'];
+addpath(utilsPath)
+addpath(analysisPath)
+
+%% plot valid-long vs invalid-long
+validLong = analyze_results(results, 'spacing', {'correct'}, ...
+    {'soa_time', 'valid'}, [.6, 1]);
+invalidLong = analyze_results(results, 'spacing', {'correct'}, ...
+    {'soa_time', 'valid'}, [.6, 0]);
+
+scatter(validLong.spacing, validLong.correct);
+hold
+scatter(invalidLong.spacing, invalidLong.correct);
+
+%% plot valid-short vs invalid-short
+validShort = analyze_results(results, 'spacing', {'correct'}, ...
+    {'soa_time', 'valid'}, [.04, 1]);
+invalidShort = analyze_results(results, 'spacing', {'correct'}, ...
+    {'soa_time', 'valid'}, [.04, 0]);
+
+scatter(validShort.spacing, validShort.correct);
+hold
+scatter(invalidShort.spacing, invlaidShort.correct);
+
+%% plot invalid-short vs valid-long
+scatter(invalidShort.spacing, invalidShort.correct);
+hold
+scatter(validLong.spacing, validLong.correct);
+
