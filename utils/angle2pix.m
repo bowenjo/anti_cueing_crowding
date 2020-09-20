ff83ac09dc1bce21d57147f7bd5b159dfaff74c3
Joel Bowen <joel_bowen@berkeley.edu>
2020-09-19 19:17:01 -0700

    minor fixes to formatting

--- utils/angle2pix.m
+++ utils/angle2pix.m
@@ -1,14 +1,13 @@
 function pix = angle2pix(subjectDistance, physicalWidth, resolution, ang)
-%converts visual angles in degrees to pixels.
-%Inputs:
-%   subjectDistance - num - subject distance to screen in cm
-%   physicalWidth - num - physical width of screen in cm
-%   resolution - num - number of pixels of display in horizontal direction
-%   ang - num - the angle in degrees to convert to pixels
- 
-pixSize = physicalWidth/resolution;  
-sz = subjectDistance*tan(pi*ang/(180));  
-pix = round(sz/pixSize);
+    %converts visual angles in degrees to pixels.
+    %   subjectDistance - num - subject distance to screen in cm
+    %   physicalWidth - num - physical width of screen in cm
+    %   resolution - num - number of pixels of display in horizontal direction
+    %   ang - num - the angle in degrees to convert to pixels
+
+    pixSize = physicalWidth/resolution;  
+    sz = subjectDistance*tan(pi*ang/(180));  
+    pix = round(sz/pixSize);
 
 return
 
